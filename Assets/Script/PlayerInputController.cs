e805bdf0f0b58a4097793420a97f7a563431ce14
whew15 <huna94@naver.com>
2024-05-14 12:07:57 +0900

    Chapter3

---
+++ Assets/Script/PlayerInputController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine.InputSystem;
+using UnityEngine;
+using System;
+
+public class PlayerInputController : TopDownController
+{
+    private Camera camera;
+    private void Awake()
+    {
+        camera = Camera.main;
+    }
+
+    public void OnMove(InputValue value)
+    {
+        Vector2 moveInput = value.Get<Vector2>().normalized;
+        CallMoveEvent(moveInput);
+    }
+    public void OnLook(InputValue value)
+    {
+        Vector2 newAim = value.Get<Vector2>();
+        Vector2 worldPos = camera.ScreenToWorldPoint(newAim);
+        newAim = (worldPos - (Vector2)transform.position).normalized;
+
+        CallLookEvent(newAim);
+    }
+}
---
+++ Assets/Script/TopDownAimRotation.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class TopDownAimRotation : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer armRenderer;
+    [SerializeField] private Transform armPivot;
+
+    [SerializeField] private SpriteRenderer characterRenderer;
+
+    private TopDownController controller;
+
+    private void Awake()
+    {
+        controller = GetComponent<TopDownController>();
+    }
+
+    private void Start()
+    {
+        controller.OnLookEvent += OnAim;
+    }
+
+    private void OnAim(Vector2 direction)
+    {
+        RotateArm(direction);
+    }
+    private void RotateArm(Vector2 direction)
+    {
+        float rotz = Mathf.Atan2 (direction.y, direction.x) * Mathf.Rad2Deg;
+
+        characterRenderer.flipX = Mathf.Abs(rotz) > 90f;
+        armPivot.rotation = Quaternion.Euler(0, 0, rotz);
+    }
+
+
+}
